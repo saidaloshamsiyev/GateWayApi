16232d5a5697486d8d8eb60cb6fe5cff481ec3ff
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-10-21 15:05:31 +0500

    gateway

--- src/main/java/uz/sb/apigateway/ApiGatewayApplication.java
+++ src/main/java/uz/sb/apigateway/ApiGatewayApplication.java
@@ -6,8 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ApiGatewayApplication {
 
+
+public class ApiGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
