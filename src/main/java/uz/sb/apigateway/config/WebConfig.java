039fefc49a641ad5028bff3f7ad4cae378d55dd1
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-02 19:58:16 +0500

    gateway

--- src/main/java/uz/sb/apigateway/config/WebConfig.java
+++ src/main/java/uz/sb/apigateway/config/WebConfig.java
@@ -5,35 +5,31 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 
 import java.util.List;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // Barcha so'rovlar uchun
-                .allowedOrigins("http://localhost:3001", "http://localhost:3000", "http://159.65.119.240:8080") // Faqat belgilangan manzilardan kelgan so'rovlar
-                .allowedMethods("GET", "POST", "PUT", "DELETE") // Qabul qilinadigan HTTP usullari
-                .allowedHeaders("*"); // Qabul qilinadigan sarlavhalar
-    }
+public class WebConfig {
 
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://159.65.119.240:8080","http://localhost:3001")); // AWS domainini qo'shing
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));  // Ruxsat etilgan metodlar
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));  // Ruxsat etilgan sarlavhalar
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));  // Javob sarlavhalarini expose qilish
-        corsConfiguration.setAllowCredentials(true);  // Cookie yoki auth ma'lumotlarini uzatishga ruxsat
+        CorsConfiguration corsConfiguration = getCorsConfiguration();
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/api/**", corsConfiguration);
         source.registerCorsConfiguration("/v3/api-docs/**", corsConfiguration); // Swagger UI uchun
         source.registerCorsConfiguration("/swagger-ui/**", corsConfiguration); // Swagger UI uchun
         return source;
+    }
+
+    private static CorsConfiguration getCorsConfiguration() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://159.65.119.240:8080", "http://localhost:3001"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+        corsConfiguration.setAllowCredentials(true);
+        return corsConfiguration;
     }
 }
