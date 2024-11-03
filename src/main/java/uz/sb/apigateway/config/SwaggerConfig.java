628d2f7367e80f6930fbbe1ae1149d5a0dd473fc
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-03 15:17:34 +0500

    gateway

--- src/main/java/uz/sb/apigateway/config/SwaggerConfig.java
+++ src/main/java/uz/sb/apigateway/config/SwaggerConfig.java
@@ -8,25 +8,23 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
-    public OpenAPI customOpenAPI() {
-        final String securitySchemeName = "bearerAuth";
+    public OpenAPI customOpenAPI(Environment environment) throws UnknownHostException, UnknownHostException {
+
+        String hostAddress = InetAddress.getLocalHost().getHostAddress();
+
+        String port = environment.getProperty("local.server.port", "8080"); // 8080 - standart port agar topilmasa
+
+        String gatewayUrl = "http://" + hostAddress + ":" + port + "/api";
+
         return new OpenAPI()
-                .addServersItem(new Server().url("http://159.65.119.240:8080/api"));
-//                .info(new Info()
-//                        .title("AUTH Service API")
-//                        .version("1.0")
-//                        .description("API for managing auth"))
-//                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
-//                .components(new Components()
-//                        .addSecuritySchemes(securitySchemeName,
-//                                new SecurityScheme()
-//                                        .name(securitySchemeName)
-//                                        .type(SecurityScheme.Type.HTTP)
-//                                        .scheme("Bearer")
-//                                        .bearerFormat("JWT")));
+                .addServersItem(new Server().url(gatewayUrl));
     }
 }
