0d09000d223a0e134d64262a710c77ccbf6147c1
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-06 20:59:11 +0500

    gateway

--- src/main/java/uz/sb/apigateway/config/WebConfig.java
+++ src/main/java/uz/sb/apigateway/config/WebConfig.java
@@ -13,18 +13,7 @@ import java.util.List;
 
 @Configuration
 public class WebConfig {
-
     @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration corsConfiguration = getCorsConfiguration();
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/api/**", corsConfiguration);
-        source.registerCorsConfiguration("/v3/api-docs/**", corsConfiguration); // Swagger UI uchun
-        source.registerCorsConfiguration("/swagger-ui/**", corsConfiguration); // Swagger UI uchun
-        return source;
-    }
-
     private static CorsConfiguration getCorsConfiguration() {
         CorsConfiguration corsConfiguration = new CorsConfiguration();
         corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://159.65.119.240:8080", "http://localhost:3001"));
@@ -34,7 +23,6 @@ public class WebConfig {
         corsConfiguration.setAllowCredentials(true);
         return corsConfiguration;
     }
-
 
 
 }
