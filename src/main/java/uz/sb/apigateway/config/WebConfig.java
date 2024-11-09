33290aa425833d8dceb6060492d709e1ff80d20a
saidaloshamsiyev <muxammadaminartikov@gmail.com>
2024-11-09 16:49:24 +0500

    gateway

--- src/main/java/uz/sb/apigateway/config/WebConfig.java
+++ src/main/java/uz/sb/apigateway/config/WebConfig.java
@@ -13,16 +13,16 @@ import java.util.List;
 
 @Configuration
 public class WebConfig {
-    @Bean
-    private static CorsConfiguration getCorsConfiguration() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://159.65.119.240:8080", "http://localhost:3001"));
-        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
-        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
-        corsConfiguration.setExposedHeaders(List.of("Authorization"));
-        corsConfiguration.setAllowCredentials(true);
-        return corsConfiguration;
-    }
+//    @Bean
+//    private static CorsConfiguration getCorsConfiguration() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://159.65.119.240:8080", "http://localhost:3001"));
+//        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+//        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
+//        corsConfiguration.setExposedHeaders(List.of("Authorization"));
+//        corsConfiguration.setAllowCredentials(true);
+//        return corsConfiguration;
+//    }
 
 
 }
